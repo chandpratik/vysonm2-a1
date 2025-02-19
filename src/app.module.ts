4ff0e3dff4ed3e5bef8dbec134162aa6b5461a1e
chandpratik <chandpratikal1@gmail.com>
2025-02-20 01:20:07 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UrlShortenerModule } from './url-shortener/url-shortener.module';
+
+@Module({
+  imports: [UrlShortenerModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
